da13b6733d13a2ab972f0a76ab2966f5f2c2850c
James Su-Shum <sushumjames@gmail.com>
2020-12-21 22:05:10 -0600

    Intro to IEnumerable, Extension Method, Lambda Expressions, Func and Action Types

---
+++ Features/MyLinq.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Features.MyLinq
+{
+    public static class MyLinq
+    {
+        // Count is a generic method denoted by type <T>. The 'this' keyword in parameter allows for use of the count method wherever there is an IEnumerable<T>
+        public static int Count<T>(this IEnumerable<T> sequence)
+        {
+            int count = 0;
+            foreach (var item in sequence)
+            {
+                count += 1;
+            }
+            return count;
+        }
+    }
+}
---
+++ Features/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+// using Features.MyLinq;
+
+namespace Features
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Func types using lambda expressions - the first generic types describe the incoming parameters and the last generic type is the return type 
+            Func<int, int> square = x => x * x;
+            Console.WriteLine(square(3));
+            Func<int, int, int> add = (x, y) => x + y;
+            Console.WriteLine(add(5, 5));
+
+            // Action type using lambda expressions - always returns void, the generic type parameter only describe the incoming parameter to the method
+            Action<int> write = x => Console.WriteLine(x);
+            write(square(add(3, 5)));
+
+            Employee[] developers = new Employee[]
+            {
+                new Employee { Id = 1, Name = "Scott"},
+                new Employee { Id = 2, Name = "Chris"}
+            };
+
+            List<Employee> sales = new List<Employee>()
+            {
+                new Employee { Id = 3, Name = "Alex"}
+            };
+
+            foreach (var employee in developers.Where(e => e.Name.Length == 5).OrderBy(e => e.Name))
+            {
+                Console.WriteLine(employee.Name);
+            }
+        }
+    }
+}
